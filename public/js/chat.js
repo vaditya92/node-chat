60bb526424c8a37f2073c43319b09ed12d41f7aa
Aditya Varma <vaditya92@gmail.com>
2018-08-15 20:44:31 +0530

    Send messages to only people in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -65,7 +65,6 @@ var messageTextBox = jQuery('[name=message]');
 jQuery("#message-form").on('submit', function(e) {
     e.preventDefault();
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     }, function() {
         messageTextBox.val('');
